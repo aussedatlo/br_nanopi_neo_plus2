3867c9927321780b72dc12617ab4b8f824bcf7b3
Louis Aussedat <aussedat.louis@gmail.com>
2020-04-28 00:09:29 +0200

    external: add variable to edit rootfs via makefile with TARGET_CONFIGURE
    This variable is a hook that allow to modify TARGET_DIR after user creation.

--- packages/external.mk
+++ packages/external.mk
@@ -1,1 +1,7 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_PACKAGES_PATH)/packages/*/*.mk))
+
+
+# Add a TARGETS_CONFIGURE before creating rootfs
+# to enable the possibility to modify rootfs via makefile
+# after user creation
+TARGETS_ROOTFS := rootfs-common $(TARGET_CONFIGURE) $(TARGETS_ROOTFS)
