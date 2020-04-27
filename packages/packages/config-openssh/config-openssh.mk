74c3f2c54e079a6e7808c84d6878410e21238fe1
Louis Aussedat <aussedat.louis@gmail.com>
2020-04-28 00:11:05 +0200

    config-openssh: new package
    This package can install a public key directly to all users created (with home folder in /home).

---
+++ packages/packages/config-openssh/config-openssh.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# config-openssh
+#
+################################################################################
+
+CONFIG_OPENSSH_USER_KEY = $(call qstrip,$(BR2_CONFIG_OPENSSH_INSTALL_KEY))
+
+config-openssh-add-key:
+	@$(call MESSAGE,"Configuring openssh")
+	@echo "INFO: installing ssh key $(CONFIG_OPENSSH_USER_KEY)"
+	@USERS=$$(find $(TARGET_DIR)/home -maxdepth 1 -type d | tail -n+2 ); \
+	for user in $$USERS; do \
+		echo "INFO: installing key for user $$(basename $$user)"; \
+		mkdir -p $$user/.ssh; \
+		rm -rf $$user/.ssh/authorized_keys; \
+		cat $(CONFIG_OPENSSH_USER_KEY) >> \
+			$$user/.ssh/authorized_keys; \
+	done
+
+ifneq ($(BR2_CONFIG_OPENSSH_INSTALL_KEY),)
+TARGETS_CONFIGURE += config-openssh-add-key
+endif
