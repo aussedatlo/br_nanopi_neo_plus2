6baf6b149043044cdf141d77784e8122e43a7b64
Louis Aussedat <aussedat.louis@gmail.com>
2020-05-29 00:01:05 +0200

    external.mk: fix user detection

--- packages/external.mk
+++ packages/external.mk
@@ -3,7 +3,13 @@ include $(sort $(wildcard $(BR2_EXTERNAL_PACKAGES_PATH)/packages/*/*.mk))
 # Add a TARGETS_CONFIGURE before creating rootfs
 # to enable the possibility to modify rootfs via makefile
 # after user creation
-TARGETS_ROOTFS := rootfs-common $(TARGET_CONFIGURE) $(TARGETS_ROOTFS)
+TARGETS_ROOTFS := rootfs-create-home $(TARGET_CONFIGURE) $(TARGETS_ROOTFS)
+
+PHONY: rootfs-create-home
+rootfs-create-home:
+	cat $(ROOTFS_USERS_TABLES) | while read -r line; do \
+		mkdir -p $(TARGET_DIR)/$$(echo $$line | cut -d " " -f6); \
+	done
 
 # 1: file, 2: fragment, 3: str condition
 # Add a fragment to a file IF str is not present
