e86637eac05da25478b713aa7919aaf6a98291c9
Louis Aussedat <aussedat.louis@gmail.com>
2020-05-30 22:52:42 +0200

    packages/lnd: fix lnd package

--- packages/packages/lnd/lnd.mk
+++ packages/packages/lnd/lnd.mk
@@ -8,25 +8,12 @@ LND_VERSION = v0.10.1-beta.rc2
 LND_SITE = https://github.com/lightningnetwork/lnd/archive
 LND_SOURCE = $(LND_VERSION).tar.gz
 
-LND_VENDOR_SOURCE = \
-https://github.com/lightningnetwork/lnd/releases/download/$(LND_VERSION)/vendor.tar.gz
-
-# LND_WORKSPACE = vendor
-
-# define LND_PRE_BUILD
-# 	wget $(LND_VENDOR_SOURCE) -O $(@D)/vendor.tar.gz
-# 	tar -xf $(@D)/vendor.tar.gz -C $(@D)
-# endef
-
 define LND_BUILD_CMDS
-	$(MAKE) build GO=$(HOST_DIR)/bin/go $(GO_TARGET_ENV) -C $(@D)
+	$(MAKE) build $(GO_TARGET_ENV) -C $(@D)
 endef
 
 define LND_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 $(@D)/lnd-debug $(TARGET_DIR)/usr/bin/lnd
-	$(INSTALL) -m 755 $(@D)/lncli-debug $(TARGET_DIR)/usr/bin/lncli
+	$(MAKE) install $(GO_TARGET_ENV) -C $(@D)
 endef
-
-LND_PRE_BUILD_HOOKS += LND_PRE_BUILD
 
 $(eval $(golang-package))
